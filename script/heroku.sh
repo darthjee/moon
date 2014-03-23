92a1d537cecc4e3f2c531d4be0e3a0c91dae66b0
darthjee <darthjee@gmail.com>
2014-03-23 14:27:18 -0700

    heroku script does more than push it

--- script/heroku.sh
+++ script/heroku.sh
@@ -5,11 +5,32 @@ push_it(){
   git push $ENV $ENV:master
 }
 
+cmd_push(){
+  if [ $1 ]; then
+    while [ $1 ]; do
+      echo push_it $1
+      shift 1
+    done
+  else
+    push_it qa
+ fi
+}
+
+cmd_help(){
+  echo "push: Pushes to server"
+}
+
+CMDS="push help"
+
 if [ $1 ]; then
-  while [ $1 ]; do
-    echo push_it $1
-    shift 1
-  done
+  CMD=$1
+  shift 1
+  
+  if (echo $CMDS | grep "\b$CMD\b" > /dev/null); then
+    cmd_$CMD $*
+  else
+    cmd_help
+  fi
 else
-  push_it qa
+  cmd_help
 fi
