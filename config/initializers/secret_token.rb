1c1b226d2f13f040bdbde73b7e56f86db292f61e
Fernando Favini <darthjee@gmail.com>
2015-06-01 12:51:16 -0700

    Restart project

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Moon::Application.config.secret_token = 'a29c891f5bc471c48af612118ec61b9057edc1c4b8f8ab76c246e3bfa64dcdf7b7e6a3834d5817d9cc86b0b0f4f830076319a6ca4d59fcae75f674a1224901fa'
