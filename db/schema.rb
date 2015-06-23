1aaebc43c4eb6c1dfe152bd52f7f2513733762db
darthjee favini <darthjee@gmail.com>
2015-06-23 11:35:27 -0300

    Add event location

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616002739) do
+ActiveRecord::Schema.define(version: 20150623132735) do
 
   create_table "marriage_events", force: true do |t|
     t.time     "time"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150616002739) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "location"
   end
 
   create_table "marriage_marriages", force: true do |t|
