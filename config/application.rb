11e8ac0fd9d3cbb614fababbd383e968be5c9059
darthjee favini <darthjee@gmail.com>
2015-06-29 00:09:51 -0300

    Make links collapsable

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Moon
     config.i18n.default_locale = 'pt-BR'
     config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     config.assets.paths << Rails.root.join("vendor","assets","bower_components", "bootstrap-sass", "assets", "fonts", "bootstrap")
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components", "bootstrap-sass", "assets", "javascripts")
 
     config.assets.precompile += [/.*?\.(eot|svg|ttf|woff)$/]
   end
