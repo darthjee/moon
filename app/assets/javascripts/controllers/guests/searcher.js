1a618ab02eedbad32b59621095f7e92a06a09e5f
darthjee favini <darthjee@gmail.com>
2015-07-11 19:21:49 -0300

    Seach guest using ui-select2

--- app/assets/javascripts/controllers/guests/searcher.js
+++ app/assets/javascripts/controllers/guests/searcher.js
@@ -2,9 +2,24 @@
   function GuestsSearcherController($http) {
     this.requester = $http;
     this.options = [{ name: 'name1', id: 1 },{ name: 'name2', id: 2 }];
+    _.bindAll(this, 'search');
   }
 
   var fn = GuestsSearcherController.prototype;
+
+  fn.search = function(query) {
+    this.requester.get('/invites/search', {
+      params: { name: query.term }
+    }).then(function(res){
+      var results = _.map(res.data, function(object) {
+        return {
+          text: object.name,
+          id: object.id
+        };
+      });
+      query.callback({ results: results });
+    });
+  };
 
   var app = angular.module('guests/searcher', []);
 
