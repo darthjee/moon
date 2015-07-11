50a29c3646de7784a7a04a47985fcaae0551c600
darthjee favini <darthjee@gmail.com>
2015-07-11 18:13:33 -0300

    Add sample search

--- app/assets/javascripts/controllers/guests/searcher.js
+++ app/assets/javascripts/controllers/guests/searcher.js
@@ -1,10 +1,12 @@
 (function(_) {
-  function GuestsSearcherController() {
+  function GuestsSearcherController($http) {
+    this.requester = $http;
+    this.options = [{ name: 'name1', id: 1 },{ name: 'name2', id: 2 }];
   }
 
   var fn = GuestsSearcherController.prototype;
 
   var app = angular.module('guests/searcher', []);
 
-  app.controller('GuestsSearcherController', [GuestsSearcherController]);
+  app.controller('GuestsSearcherController', ['$http', GuestsSearcherController]);
 })(_);
