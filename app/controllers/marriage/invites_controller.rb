4bab0491cfb7895455ebdbeab033cc793380e59a
darthjee favini <darthjee@gmail.com>
2015-07-11 18:50:32 -0300

    Add search guest route

--- app/controllers/marriage/invites_controller.rb
+++ app/controllers/marriage/invites_controller.rb
@@ -3,4 +3,18 @@ class Marriage::InvitesController < ApplicationController
 
   def index
   end
+
+  def search
+    render json: guests_found
+  end
+
+  private
+
+  def guests_found
+    marriage.guests.where('name LIKE ?', "%#{name}%").pluck(:id, :name).map{ |i| i.as_hash([:id, :name]) }
+  end
+
+  def name
+    params.require(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   namespace :marriage, path: '/' do
     get '/' => 'marriage#show', as: :home
 
-    resources :invites, only: [:index]
+    resources :invites, only: [:index] do
+      get :search, on: :collection
+    end
   end
 end
