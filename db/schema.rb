ed1dc3dabbe775def0403958c01eaa829b850467
darthjee favini <darthjee@gmail.com>
2015-07-27 18:40:34 -0300

    Make presence boolean

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150629034030) do
+ActiveRecord::Schema.define(version: 20150727213243) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150629034030) do
     t.integer  "marriage_id"
     t.integer  "invite_id"
     t.string   "name"
-    t.string   "presence"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "presence"
   end
 
   create_table "marriage_invites", force: true do |t|
