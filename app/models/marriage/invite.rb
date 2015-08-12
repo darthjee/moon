1ca9cd292ba3a263798165b5fdaa0a0e347089da
darthjee favini <darthjee@gmail.com>
2015-08-12 14:27:31 -0300

    Add unique code

--- app/models/marriage/invite.rb
+++ app/models/marriage/invite.rb
@@ -1,4 +1,17 @@
 class Marriage::Invite < ActiveRecord::Base
   belongs_to :marriage
   has_many :guests
+
+  def start_code
+    self.code = build_code until unique_code?
+    save
+  end
+
+  def build_code
+    SecureRandom.hex(5)
+  end
+
+  def unique_code?
+    !marriage.invites.exists?(code: code)
+  end
 end
