b890bf97e47f24f93206014e5717ac131e09dae6
darthjee favini <darthjee@gmail.com>
2015-08-16 18:05:11 -0300

    Use a more clean syntax to invite controller

--- app/assets/javascripts/controllers/guests/invite.js
+++ app/assets/javascripts/controllers/guests/invite.js
@@ -1,9 +1,8 @@
 (function(_) {
   function InviteController(service, notifier) {
     this.service = service;
-    this.guest_info = {};
     this.selected = {};
-    this.invite_info = {};
+    this.invite = {};
 
     _.bindAll(this, '_parseResponse', 'setInvite');
     notifier.register('select-invite', this.setInvite);
@@ -12,33 +11,17 @@
   var fn = InviteController.prototype;
       app = angular.module('guests/invite', ['notifier', 'invites/service']);
 
-  fn.setInvite = function(id) {
-    this.selected = id;
-  };
+  fn.setInvite = function(selected) {
+    this.selected = selected;
 
-  fn.guest = function() {
-    if (this.selected.id === this.guest_info.id) {
-      return this.guest_info;
-    } else {
-      this.guest_info = this.selected;
+    if (selected && selected.id) {
       this._fetch();
-      return this.selected;
-    }
-  };
-
-  fn.invite = function() {
-    if (this.selected.id === this.guest_info.id) {
-      return this.invite_info;
-    } else {
-      this.guest_info = this.selected;
-      this._fetch();
-      return this.invite_info;
     }
   };
 
   fn.update = function() {
-    var id = this.invite_info.id,
-        guests = this.invite_info.guests;
+    var id = this.invite.id,
+        guests = this.invite.guests;
 
     this.service.update(id, {
       guests: guests
@@ -55,8 +38,7 @@
     var invite = data.invite,
         guest = data.guest;
 
-    this.guest_info = guest;
-    this.invite_info = invite;
+    this.invite = invite;
 
     invite.guests = invite.guests.expandSize(invite.invites);
   };
