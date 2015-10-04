933aacc614bee5707437c90fcfe465b6dbd7e720
darthjee favini <darthjee@gmail.com>
2015-10-04 19:23:39 -0300

    Start codes on invite creation

--- app/models/marriage/invite.rb
+++ app/models/marriage/invite.rb
@@ -1,40 +1,50 @@
 class Marriage::Invite < ActiveRecord::Base
   belongs_to :marriage
   has_many :guests
+  before_create :start_codesss
 
   validates :email, email: true, if: -> { email.present? }
 
   scope :search_label, proc { |label| where('label ILIKE ?', "%#{label}%") }
 
   def start_code(length = 2)
-    self.code = build_code(length) until unique_code?
+    start_random_attribute(:code, length)
     save
   end
 
   def start_authentication_token(length = 8)
-    self.authentication_token = build_code(length) until unique_token?
+    start_random_attribute(:authentication_token, length)
     save
   end
 
   private
+
+  def start_random_attribute(attribute, length)
+    public_send("#{attribute}=", build_code(length)) until unique_attribute?(attribute, public_send(attribute))
+  end
 
   def build_code(length)
     SecureRandom.hex(length)
   end
 
   def unique_code?
-    other_like?(:code, code)
+    unique_attribute?(:code, code)
   end
 
   def unique_token?
-    other_like?(:authentication_token, authentication_token)
+    unique_attribute?(:authentication_token, authentication_token)
   end
 
-  def other_like?(key, value)
+  def unique_attribute?(key, value)
     !other_invites.exists?(key => value) if value
   end
 
   def other_invites
     marriage.invites.where('id != ?', id)
+  end
+
+  def start_codesss
+    start_random_attribute(:code, 2)
+    start_random_attribute(:authentication_token, 8)
   end
 end
