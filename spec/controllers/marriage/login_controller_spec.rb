69f5e64a2427300b8350efd3219c0f1c50aad9c4
darthjee favini <darthjee@gmail.com>
2015-10-11 09:56:16 -0300

    Add specs over is_logged behavior

--- spec/controllers/marriage/login_controller_spec.rb
+++ spec/controllers/marriage/login_controller_spec.rb
@@ -1,2 +1,47 @@
+require 'spec_helper'
+
 describe Marriage::LoginController do
+  let(:user) { marriage_invites(:first) }
+
+  describe 'GET check' do
+    context 'User is not logged' do
+      it do
+        get :check, format: :json
+        expect(response).to be_a_not_found
+      end
+
+      context 'Because user has the wrong credentials' do
+        before do
+          cookies.signed[:credentials] = 'wrong'
+        end
+
+        it do
+          get :check, format: :json
+          expect(response).to be_a_not_found
+        end
+      end
+
+      context 'Because user has a fake credential' do
+        before do
+          cookies[:credentials] = user.authentication_token
+        end
+
+        it do
+          get :check, format: :json
+          expect(response).to be_a_not_found
+        end
+      end
+    end
+
+    context 'User is logged' do
+      before do
+        cookies.signed[:credentials] = user.authentication_token
+      end
+
+      it do
+        get :check, format: :json
+        expect(response).to be_a_success
+      end
+    end
+  end
 end
