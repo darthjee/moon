c88031abdac2ff39b1723c36701495e726cd2ffb
darthjee favini <darthjee@gmail.com>
2015-10-23 00:44:24 -0200

    Add guest factory

--- spec/controllers/marriage/best_man_controller_spec.rb
+++ spec/controllers/marriage/best_man_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe Marriage::BestManController do
+  let(:response_json) { JSON.parse response.body }
+
   describe 'GET index' do
     let(:user) { marriage_invites(:first) }
     let(:invite) { user }
     let(:parameters) { { format: format } }
-    let(:response_json) { JSON.parse response.body }
 
     context 'When user is logged' do
       before do
@@ -39,6 +40,51 @@ describe Marriage::BestManController do
           get :index, parameters
           expect(response.status).to eq(404)
         end
+      end
+    end
+  end
+
+  describe 'GET show_maids' do
+    let(:parameters) do
+      {
+        format: :json,
+        role: role
+      }
+    end
+    let(:marriage) { marriage_marriages(:first) }
+    before do
+      Marriage::Guest.update_all(role: nil)
+      marriage.invites.each do |invite|
+        create(:guest, role: 'maid_honor', invite: invite)
+      end
+      create(:guest, role: 'best_man', invite: marriage.invites.first)
+      2.times { create(:guest, role: 'mother', invite: marriage.invites.first) }
+    end
+
+    context 'when requesting the maids' do
+      let(:role) { 'maid_honor' }
+
+      it 'returns all the maids' do
+        get :show_maids, parameters
+        expect(response_json.map { |g| g['role'] }).to eq(marriage.invites.count.times.map { role })
+      end
+    end
+
+    context 'when requesting the best_man' do
+      let(:role) { 'best_man' }
+
+      it 'returns all the best_man' do
+        get :show_maids, parameters
+        expect(response_json.map { |g| g['role'] }).to eq([role])
+      end
+    end
+
+    context 'when requesting the mother' do
+      let(:role) { 'mother' }
+
+      it 'returns all the mothers' do
+        get :show_maids, parameters
+        expect(response_json.map { |g| g['role'] }).to eq([role, role])
       end
     end
   end
