40168a970dfd576266a7082af94813ed2d2a32e1
darthjee favini <darthjee@gmail.com>
2015-11-10 17:23:19 -0200

    Welcome receive user

--- app/models/mandrill/request/welcome.rb
+++ app/models/mandrill/request/welcome.rb
@@ -1,10 +1,14 @@
 class Mandrill::Request::Welcome < Mandrill::Request
+  attr_reader :user
+  delegate :name, :email, to: :user
+
   TEMPLATE_KEY = 'welcome'
 
-  def initialize(email)
+  def initialize(user)
+    @user = user
     super({
-      recepient: email,
-      data: {}
+      recepient: { email: email, name: :name },
+      data: { NAME: name }
     }, self.class::TEMPLATE_KEY)
   end
 end
--- spec/models/mandrill/request/welcome_spec.rb
+++ spec/models/mandrill/request/welcome_spec.rb
@@ -1,12 +1,20 @@
 require 'spec_helper'
 
 describe Mandrill::Request::Welcome do
-  let(:email) { 'user@srv.com' }
-  let(:subject) { described_class.new(email) }
+  let(:invite) { marriage_invites(:first) }
+  let(:name) { invite.guests.first.name }
+  let(:user) { User.find(invite.id) }
+  let(:subject) { described_class.new(user) }
 
   describe '#template_name' do
     it do
       expect(subject.template_name).to eq('welcome_template')
+    end
+  end
+
+  describe '#data' do
+    it do
+      expect(subject.messages.first.data.as_json).to eq( 'NAME' => name )
     end
   end
 end
