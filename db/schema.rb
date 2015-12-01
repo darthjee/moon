7fa8ec5f2b0adc4c28ff9ae2dd162d4031c6094f
darthjee favini <darthjee@gmail.com>
2015-12-01 11:08:43 -0200

    Add gift link

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129004248) do
+ActiveRecord::Schema.define(version: 20151130221944) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 20151129004248) do
     t.datetime "updated_at"
     t.string   "location"
     t.string   "address"
+  end
+
+  create_table "marriage_gift_links", force: true do |t|
+    t.integer  "gift_id"
+    t.integer  "store_list_id"
+    t.string   "url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "marriage_gifts", force: true do |t|
