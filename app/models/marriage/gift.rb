545a381dafcea46c52e9a55e7fb15e0fbdd69253
darthjee favini <darthjee@gmail.com>
2015-12-02 10:55:12 -0200

    Add relation between gifts and marriages

--- app/models/marriage/gift.rb
+++ app/models/marriage/gift.rb
@@ -1,3 +1,4 @@
 class Marriage::Gift < ActiveRecord::Base
   has_many :gift_links
+  belongs_to :marriage
 end
--- app/models/marriage/marriage.rb
+++ app/models/marriage/marriage.rb
@@ -2,6 +2,7 @@ class Marriage::Marriage < ActiveRecord::Base
   has_many :events
   has_many :invites
   has_many :store_lists
+  has_many :gifts
 
   def guests
     ::Marriage::Guest.joins(:invite).where(marriage_invites: { marriage_id: id })
