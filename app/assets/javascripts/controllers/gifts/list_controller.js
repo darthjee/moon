e07cac559638be1ce7aee5b44323e74dde66f270
darthjee favini <darthjee@gmail.com>
2015-12-02 11:29:20 -0200

    Load gifts on load up

--- app/assets/javascripts/controllers/gifts/list_controller.js
+++ app/assets/javascripts/controllers/gifts/list_controller.js
@@ -2,11 +2,23 @@
   function GiftsListController(giftsService) {
     this.service = giftsService;
 
-    this.loaded = true;
+    _.bindAll(this, '_parseGifts');
+
+    this.loadGifts();
+
   }
 
   var fn = GiftsListController.prototype;
       app = angular.module('gifts/list_controller', ['gifts/service']);
+
+  fn.loadGifts = function() {
+    this.service.loadGifts().success(this._parseGifts);
+  };
+
+  fn._parseGifts = function(data) {
+    this.gifts = data;
+    this.loaded = true;
+  };
 
   app.controller('GiftsListController', ['giftsService', GiftsListController]);
 })(window._);
--- app/assets/javascripts/services/gifts/service.js
+++ app/assets/javascripts/services/gifts/service.js
@@ -10,5 +10,9 @@
   var fn = GiftsService.prototype,
       module = angular.module('gifts/service', []);
 
+  fn.loadGifts = function() {
+    return this.requester.get('/presentes.json');
+  };
+
   module.service('giftsService', ['$http', GiftsServiceFactory]);
 })(window._);
