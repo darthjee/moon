afe3a4492362e3cc353903e311d6242077f3cc94
darthjee favini <darthjee@gmail.com>
2015-12-04 00:17:35 -0200

    Fill link with store name

--- app/models/marriage/gift_link.rb
+++ app/models/marriage/gift_link.rb
@@ -1,12 +1,11 @@
 class Marriage::GiftLink < ActiveRecord::Base
   belongs_to :gift
   belongs_to :store_list
-
-  def store_image_url
-    store_list.store.image_url
-  end
+  delegate :store, to: :store_list
 
   def as_json(*args)
-    super(*args).merge(store_image_url: store_image_url)
+    super(*args).merge(
+      store: store.as_json(only: [:image_url, :name])
+    )
   end
 end
