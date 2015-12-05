c0e037add310813adc46d53237d8227b30e7173b
darthjee favini <darthjee@gmail.com>
2015-12-05 19:28:08 -0200

    Add more specs for gifts creation

--- spec/controllers/marriage/gits_controller_spec.rb
+++ spec/controllers/marriage/gits_controller_spec.rb
@@ -7,10 +7,42 @@ describe Marriage::GiftsController do
 
   describe 'POST create' do
     let(:parameters_key) { 'create' }
+    let(:last_link) { Marriage::GiftLink.last }
 
     it do
       post :create, parameters
       expect(response).to be_success
+    end
+
+    it do
+      expect do
+        post :create, parameters
+      end.to change(Marriage::Gift, :count)
+    end
+
+    it 'creates a gift for the given parameters' do
+      post :create, parameters
+
+      expect(Marriage::Gift.last.attributes.slice('image_url', 'name')).to eq(
+        'image_url' => 'http://image_url.com',
+        'name' => 'Gift Name'
+      )
+    end
+
+    it do
+      expect do
+        post :create, parameters
+      end.to change(Marriage::GiftLink, :count)
+    end
+
+    it 'creates the correct gift link' do
+      post :create, parameters
+
+      expect(last_link.attributes.slice('url', 'store_list_id', 'gift_id')).to eq({
+        'url' => 'http://gifturl',
+        'store_list_id' => 1,
+        'gift_id' => Marriage::Gift.last.id
+      })
     end
   end
 end
