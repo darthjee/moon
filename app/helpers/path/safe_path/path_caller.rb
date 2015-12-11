7c064685eb1c560e52673bc1ef02e2cce92d79ca
darthjee favini <darthjee@gmail.com>
2015-12-11 00:03:56 -0200

    Use underscore key identifier

--- app/helpers/path/safe_path/path_caller.rb
+++ app/helpers/path/safe_path/path_caller.rb
@@ -10,7 +10,7 @@ class Path::SafePath::PathCaller
   def path
     controller_path.tap do |path|
       keys_map.each do |key, key_s|
-        regexp = Regexp.new("#{key_s}\\b")
+        regexp = Regexp.new("\\b#{key_s}\\b")
         path.gsub!(regexp, args[key])
       end
     end
@@ -19,7 +19,7 @@ class Path::SafePath::PathCaller
   private
 
   def key_names
-    @keys ||= args.keys.map { |k| ":#{k}" }
+    @keys ||= args.keys.map { |k| "_#{k}" }
   end
 
   def keys_map
