1cdc0a2f2d523691bf572e74077e20ac350bad07
darthjee favini <darthjee@gmail.com>
2015-12-12 00:10:06 -0200

    Skip canceled gifts

--- app/models/marriage/gift.rb
+++ app/models/marriage/gift.rb
@@ -2,6 +2,10 @@ class Marriage::Gift < ActiveRecord::Base
   has_many :gift_links
   belongs_to :marriage
 
+  default_scope do
+    where.not(status: :canceled)
+  end
+
   def as_json(*args)
     super(*args).merge(
       gift_links: gift_links.map(&:as_json),
