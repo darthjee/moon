1e310d2bcd0e2b2246ec320643b3d44d50768f74
darthjee favini <darthjee@gmail.com>
2015-12-12 00:06:30 -0200

    Use symbol

--- app/models/marriage/gift/paginator.rb
+++ app/models/marriage/gift/paginator.rb
@@ -38,7 +38,7 @@ class Marriage::Gift::Paginator
   end
 
   def ordered_gifts
-    gifts.order(order_by => sort_direction).order(name: :asc)
+    gifts.order(order_by => sort_direction.to_sym).order(name: :asc)
   end
 
   def gifts
