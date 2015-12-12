1d71069ddb7d4d8e5bf9debc644262417eb7d057
darthjee favini <darthjee@gmail.com>
2015-12-11 23:25:19 -0200

    Add more fields to gift

---
+++ db/migrate/20151211160310_add_missing_attributes_to_marriage_gifts.rb
@@ -0,0 +1,13 @@
+class AddMissingAttributesToMarriageGifts < ActiveRecord::Migration
+  def change
+    add_column :marriage_gifts, :package, :integer, default: 1, null: false
+    add_column :marriage_gifts, :bought, :integer, default: 0, null: false
+    add_column :marriage_gifts, :status, :string, default: :open
+  end
+
+  def up
+    Marriage::Gift.where.not(quantity: nil).uniq.pluck(:quantity).each do |quantity|
+      Marriage::Gift.where(quantity: quantity).update_all(package: quantity)
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151210123707) do
+ActiveRecord::Schema.define(version: 20151211160310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -67,9 +67,12 @@ ActiveRecord::Schema.define(version: 20151210123707) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "marriage_id"
-    t.integer  "quantity",    default: 1,   null: false
-    t.float    "min_price",   default: 0.0, null: false
-    t.float    "max_price",   default: 0.0, null: false
+    t.integer  "quantity",    default: 1,      null: false
+    t.float    "min_price",   default: 0.0,    null: false
+    t.float    "max_price",   default: 0.0,    null: false
+    t.integer  "package",     default: 1,      null: false
+    t.integer  "bought",      default: 0,      null: false
+    t.string   "status",      default: "open"
   end
 
   create_table "marriage_guests", force: true do |t|
