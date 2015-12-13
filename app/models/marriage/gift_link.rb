677c195c93b159a680332b45fdf576ccc3499e47
darthjee favini <darthjee@gmail.com>
2015-12-13 01:23:17 -0200

    Add status to gift link

--- app/models/marriage/gift_link.rb
+++ app/models/marriage/gift_link.rb
@@ -3,6 +3,10 @@ class Marriage::GiftLink < ActiveRecord::Base
   belongs_to :store_list, class_name: 'Store::List', foreign_key: :store_list_id
   belongs_to :account, class_name: 'Bank::Account'
 
+  default_scope do
+    where.not(status: :canceled)
+  end
+
   def as_json(*args)
     super(*args).merge(
       store: store.as_json(only: [:image_url, :name, :bg_color]),
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211160310) do
+ActiveRecord::Schema.define(version: 20151213032201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20151211160310) do
     t.datetime "updated_at"
     t.float    "price"
     t.integer  "account_id"
+    t.string   "status",        default: "open"
   end
 
   create_table "marriage_gifts", force: true do |t|
