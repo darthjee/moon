c4588d86f81edbf91a673d2d8418ac1e5eb338ef
darthjee favini <darthjee@gmail.com>
2015-12-13 21:26:46 -0200

    Extract priceless gift to model

--- app/assets/javascripts/models/gift.js
+++ app/assets/javascripts/models/gift.js
@@ -1,15 +1,23 @@
 (function(_, undefined) {
+  function GiftFactory(gift) {
+    return Gift;
+  }
+
   function Gift(gift) {
     _.extend(this, gift);
 
     this.priceless = this.price_range.length <= 0 || this.display_type == 'priceless';
   }
 
-  function GiftFactory(gift) {
-    return Gift;
-  }
+  var fn = Gift.prototype,
+      module = angular.module('gifts/gift', []);
 
-  var module = angular.module('gifts/gift', []);
+  fn.is_priceless = function() {
+    if (this.priceless == undefined) {
+      this.priceless = his.price_range.length <= 0 || this.display_type == 'priceless';
+    }
+    return this.priceless;
+  };
 
   module.factory('Gift', [GiftFactory]);
 })(_);
