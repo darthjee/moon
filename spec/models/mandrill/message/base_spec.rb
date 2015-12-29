058ed8f0c1e5552b3e4238b29661f39c395ecd39
darthjee favini <darthjee@gmail.com>
2015-12-29 19:40:04 -0200

    Test base message

---
+++ spec/models/mandrill/message/base_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Mandrill::Message::Base do
+  let(:user) { User.find(1) }
+  let(:root_url) { 'http://teste.com' }
+  let(:subject) { described_class.new(user, root_url) }
+  let(:email) { 'user@server.com' }
+  let(:name) { 'Mr. Test' }
+
+  describe '#as_json' do
+    let(:expected) do
+      {
+        rcpt: email,
+        vars: [
+          { name: 'NAME', content: name },
+          { name: 'ROOT_URL', content: root_url }
+        ]
+      }.deep_stringify_keys
+    end
+
+    it 'returns mandrill formatted message' do
+      expect(subject.as_json).to eq(expected)
+    end
+  end
+end
