2ae01c7d912f0c12ed7a5f2a33bf428138a25e4d
darthjee favini <darthjee@gmail.com>
2015-12-29 19:19:57 -0200

    Overwritte template key

--- app/models/mandrill/request/base.rb
+++ app/models/mandrill/request/base.rb
@@ -2,15 +2,19 @@ class Mandrill::Request::Base < Mandrill::Request
   attr_reader :user, :root_url
   delegate :name, :email, to: :user
 
-  def initialize(user, root_url)
+  def initialize(user, root_url, settings = {})
     @user = user
     @root_url = root_url
-    super(nil, self.class::TEMPLATE_KEY)
+    @settings = settings
   end
 
   def messages
     [
       Mandrill::Message::Base.new(user, root_url)
     ]
+  end
+
+  def template_key
+    self.class::TEMPLATE_KEY
   end
 end
