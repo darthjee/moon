e71cb5c48bdf4ef780ce72c9ea1325de914b3097
darthjee favini <darthjee@gmail.com>
2015-12-29 19:49:25 -0200

    Add password request

---
+++ app/models/mandrill/request/password.rb
@@ -0,0 +1,9 @@
+class Mandrill::Request::Password < Mandrill::Request::Base
+  TEMPLATE_KEY = 'password-recovery'
+
+  def messages
+    [
+      Mandrill::Message::Password.new(user, root_url)
+    ]
+  end
+end
