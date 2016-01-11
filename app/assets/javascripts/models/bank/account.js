8fb6274f9939df801ae23eabe9cbad976e22fa6f
darthjee favini <darthjee@gmail.com>
2016-01-11 20:17:05 -0200

    Show account type

--- app/assets/javascripts/models/bank/account.js
+++ app/assets/javascripts/models/bank/account.js
@@ -5,6 +5,9 @@
 
   function Account(account) {
     _.extend(this, account);
+
+    this.savings = this.account_type == 'savings';
+    this.checkings = this.account_type == 'checkings';
   }
 
   var fn = Account.prototype,
