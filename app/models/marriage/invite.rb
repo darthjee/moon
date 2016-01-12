7159495c77e3e31a79225775c5e4d6afea88756b
darthjee favini <darthjee@gmail.com>
2016-01-12 00:18:27 -0200

    Extract user methods from invite

--- app/models/marriage/invite.rb
+++ app/models/marriage/invite.rb
@@ -2,7 +2,6 @@ class Marriage::Invite < ActiveRecord::Base
   belongs_to :marriage
   has_many :guests
   belongs_to :user, class_name: 'User'
-  before_create :start_codesss
 
   validates :email, email: true, if: -> { email.present? }
 
@@ -10,50 +9,4 @@ class Marriage::Invite < ActiveRecord::Base
   scope :created, proc { where(status: :created) }
   default_scope { where.not(status: :cancelled) }
 
-  def self.login(email, password)
-    where.not(email: nil, password: nil).find_by(email: email, password: encrypt(password))
-  end
-
-  def password=(pass)
-    super(self.class.encrypt(pass))
-  end
-
-  def start_code(length = 2)
-    start_random_attribute(:code, length)
-    save
-  end
-
-  def start_authentication_token(length = 8)
-    start_random_attribute(:authentication_token, length)
-    save
-  end
-
-  private
-
-  def start_random_attribute(attribute, length)
-    public_send("#{attribute}=", build_code(length)) until unique_attribute?(attribute)
-  end
-
-  def build_code(length)
-    SecureRandom.hex(length)
-  end
-
-  def unique_attribute?(attribute)
-    value = public_send(attribute)
-    !other_invites.exists?(attribute => value) if value
-  end
-
-  def other_invites
-    marriage.invites.where('id != ?', id)
-  end
-
-  def start_codesss
-    start_random_attribute(:code, 2)
-    start_random_attribute(:authentication_token, 8)
-  end
-
-  def self.encrypt(pass)
-    return unless pass.present?
-    Digest::SHA256.hexdigest pass
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,57 @@
 class User < ActiveRecord::Base
   has_one :invite, class_name: 'Marriage::Invite', foreign_key: :user_id
+  before_create :start_codesss
 
   class << self
     def for_invite(invite)
-      new(invite: invite)
+      invite.user
     end
+  end
+
+  def self.login(email, password)
+    where.not(email: nil, password: nil).find_by(email: email, password: encrypt(password))
+  end
+
+  def password=(pass)
+    super(self.class.encrypt(pass))
+  end
+
+  private
+
+  def start_codesss
+    start_random_attribute(:code, 2)
+    start_random_attribute(:authentication_token, 8)
+  end
+
+  def start_code(length = 2)
+    start_random_attribute(:code, length)
+    save
+  end
+
+  def start_authentication_token(length = 8)
+    start_random_attribute(:authentication_token, length)
+    save
+  end
+
+  def start_random_attribute(attribute, length)
+    public_send("#{attribute}=", build_code(length)) until unique_attribute?(attribute)
+  end
+
+  def build_code(length)
+    SecureRandom.hex(length)
+  end
+
+  def unique_attribute?(attribute)
+    value = public_send(attribute)
+    !other_users.exists?(attribute => value) if value
+  end
+
+  def other_users
+    self.class.where('id != ?', id)
+  end
+
+  def self.encrypt(pass)
+    return unless pass.present?
+    Digest::SHA256.hexdigest pass
   end
 end
