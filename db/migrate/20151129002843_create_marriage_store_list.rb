9d69660114173eaaacc20f5355b762bf8a098614
darthjee favini <darthjee@gmail.com>
2016-01-15 00:41:40 -0200

    Undu wrong commit

--- db/migrate/20151129002843_create_marriage_store_list.rb
+++ db/migrate/20151129002843_create_marriage_store_list.rb
@@ -2,6 +2,7 @@ class CreateMarriageStoreList < ActiveRecord::Migration
   def change
     create_table :marriage_store_lists do |t|
       t.integer :store_id
+      t.integer :marriage_id
       t.string :url
       t.timestamps
     end
