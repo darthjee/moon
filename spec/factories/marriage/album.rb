97a36bc13a131ea5d17ec61a64eff86427be01d8
darthjee favini <darthjee@gmail.com>
2016-01-21 17:15:50 -0200

    Add missing factories

---
+++ spec/factories/marriage/album.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :album, class: Marriage::Album do
+    name 'The pictures'
+    marriage
+  end
+end
--- spec/factories/marriage/gift.rb
+++ spec/factories/marriage/gift.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :gift, class: Marriage::Gift do
-    name 'The Gift'
+    sequence(:name) { |n| 'The Gift %04d' % n }
     image_url 'http://store.com/gifts/images/gift.gif'
     description 'The awesome gift'
     marriage
