a1f5d40d7d7598e94555962764af5e651dc3add9
darthjee favini <darthjee@gmail.com>
2016-01-22 12:24:59 -0200

    Add spec over gifts ordering

--- spec/models/marriage/gift/paginator_spec.rb
+++ spec/models/marriage/gift/paginator_spec.rb
@@ -34,11 +34,86 @@ describe Marriage::Gift::Paginator do
         expect(gifts_json).to eq(first_gifts.as_json)
       end
 
-      context 'when requesting another page' do
+      context 'when requesting last page' do
         let(:page) { 3 }
 
-        it 'returns the first page gifts only' do
+        it 'returns the last page gifts only' do
           expect(gifts_json).to eq(last_gifts.as_json)
+        end
+      end
+
+      context 'when there are gifts to be ordered by name' do
+        let(:real_first_gifts) { per_page.times.map { create(:gift, marriage: marriage, name: 'aaa') } }
+        let(:last_gifts) { 2.times.map { create(:gift, marriage: marriage, name: 'zzz') } }
+
+        before do
+          real_first_gifts.each(&:thread)
+        end
+
+        it 'returns the first page gifts ordered by name' do
+          expect(gifts_json).to match_array(real_first_gifts.as_json)
+        end
+
+        context 'when requesting last page' do
+          let(:page) { 4 }
+
+          it 'returns the last page gifts only ordered by name' do
+            expect(gifts_json).to match_array(last_gifts.as_json)
+          end
+        end
+
+        context 'when requesting for specific ordering' do
+          let(:params) do
+            { per_page: per_page, page: page, sort_direction: direction, sort_by: sort_by}
+          end
+
+          context 'when oredering by name' do
+            let(:sort_by) { 'name' }
+
+            context 'descending' do
+              let(:direction) { 'desc' }
+              let(:per_page) { 2 }
+
+              it 'returns first page ordered by name' do
+                expect(gifts_json).to match_array(last_gifts.as_json)
+              end
+            end
+
+            context 'ascending' do
+              let(:direction) { 'asc' }
+
+              it 'returns first page ordered by name' do
+                expect(gifts_json).to match_array(real_first_gifts.as_json)
+              end
+            end
+          end
+
+          context 'when oredering by price' do
+            let(:sort_by) { 'price' }
+            before do
+              marriage.gifts.order(:id).each.with_index do |g, i|
+                g.update(min_price: i, max_price: 1000 - i)
+              end
+              first_gifts.each(&:reload)
+              last_gifts.each(&:reload)
+            end
+
+            context 'descending' do
+              let(:direction) { 'desc' }
+
+              it 'returns first page ordered by name' do
+                expect(gifts_json).to match_array(first_gifts.as_json)
+              end
+            end
+
+            context 'ascending' do
+              let(:direction) { 'asc' }
+
+              it 'returns first page ordered by name' do
+                expect(gifts_json).to match_array(first_gifts.as_json)
+              end
+            end
+          end
         end
       end
     end
