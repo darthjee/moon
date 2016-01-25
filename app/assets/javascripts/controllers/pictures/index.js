a3666b2ec69acc6f81df2ef9cded934704421641
darthjee favini <darthjee@gmail.com>
2016-01-24 22:04:18 -0200

    Parse pictures

--- app/assets/javascripts/controllers/pictures/index.js
+++ app/assets/javascripts/controllers/pictures/index.js
@@ -5,6 +5,8 @@
     this.album_id = $routeParams.album_id;
     this.params = $routeParams;
 
+    _.bindAll(this, '_parsePictures');
+
     this.loadPictures();
   }
 
@@ -16,7 +18,34 @@
   };
 
   fn._parsePictures = function(data) {
-    console.info(data);
+    this.pictures = data.pictures;
+    this.pages = data.pages;
+    this.pagination = this.buildPagination(data);
+    this.page = data.page;
+    this.loaded = true;
+  };
+
+  fn.buildPagination = function(data) {
+    var current = data.page,
+        that = this, list;
+
+    list = _.map(new Array(data.pages), function(_, index) {
+      var page =  index + 1;
+      if (that.isPageListable(page, data.pages, current, 3)) {
+        return page;
+      }
+      return null;
+    });
+
+    return _.squeeze(list);
+  };
+
+  fn.isPageListable = function(page, total, current, blockSize) {
+    return page <= blockSize ||
+           page > total - blockSize ||
+           Math.abs(page - current) < blockSize ||
+           (Math.abs(page - current) <= blockSize && page <= (blockSize+1)) ||
+           (Math.abs(page - current) <= blockSize && page >= total - blockSize);
   };
 
   app.controller('PicturesListController', [
