d45d52b52f88c0d7a60840e0785d56ef3e3002dd
darthjee favini <darthjee@gmail.com>
2016-01-29 18:09:24 -0200

    Add login capability

--- app/assets/javascripts/controllers/admin/login.js
+++ app/assets/javascripts/controllers/admin/login.js
@@ -1,11 +1,31 @@
 (function(_, angular) {
-  function AdminLoginEditController() {
+  function AdminLoginEditController(service) {
+    this.login = {};
+
+    this.service = service;
+
+    _.bindAll(this, '_logiSuccess', '_logiSuccess');
   }
 
   var fn = AdminLoginEditController.prototype,
-      app = angular.module('admin/login/edit', []);
+      app = angular.module('admin/login/edit', ['admin/service']);
+
+  fn.performLogin = function() {
+    var promisse = this.service.login(this.login.password);
+
+    promisse.success(this._logiSuccess);
+    promisse.error(this._logiFailure);
+  };
+
+  fn._logiSuccess = function() {
+    this.logged = true;
+  };
+
+  fn._logiFailure = function() {
+    this.logged = false;
+  };
 
   app.controller('AdminLoginEditController', [
-    AdminLoginEditController
+    'adminService', AdminLoginEditController
   ]);
 })(window._, window.angular);
---
+++ app/assets/javascripts/services/admin/service.js
@@ -0,0 +1,18 @@
+(function(_, angular, undefined) {
+  function AdminServiceFactory($http) {
+    return new AdminService($http);
+  }
+
+  function AdminService($http) {
+    this.requester = $http;
+  }
+
+  var fn = AdminService.prototype,
+      module = angular.module('admin/service', []);
+
+  fn.login = function(code) {
+    return this.requester.get('/admin?code=' + code);
+  };
+
+  module.service('adminService', ['$http', AdminServiceFactory]);
+})(window._, window.angular);
