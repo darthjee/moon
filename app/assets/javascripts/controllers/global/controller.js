b9e166571f9e0d36ad9276f217288ca183f94406
darthjee favini <darthjee@gmail.com>
2016-02-02 18:39:30 -0200

    Add debug for admin

--- app/assets/javascripts/controllers/global/controller.js
+++ app/assets/javascripts/controllers/global/controller.js
@@ -1,11 +1,26 @@
 (function(_, angular) {
-  function GlobalController() {
+  function GlobalController(notifier) {
+    this.notifier = notifier;
+
+    _.bindAll(this, 'loginAdmin');
+
+    this.bind();
   }
 
   var fn = GlobalController.prototype,
-      app = angular.module('global/controller', []);
+      app = angular.module('global/controller', ['global/notifier']);
+
+  fn.bind = function() {
+    this.notifier.register('login-admin', this.loginAdmin);
+  };
+
+  fn.loginAdmin = function() {
+    this.admin = {
+      name: 'Admin'
+    };
+  };
 
   app.controller('GlobalController', [
-    GlobalController
+    'notifier', GlobalController
   ]);
 })(window._, window.angular);
