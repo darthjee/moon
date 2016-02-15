121147afb83096fb3f281d84002c0b752437cfb7
darthjee <darthjee@gmail.com>
2016-02-15 15:30:54 -0200

    Show first picture on album

--- app/models/marriage/album.rb
+++ app/models/marriage/album.rb
@@ -1,4 +1,16 @@
 class Marriage::Album < ActiveRecord::Base
   belongs_to :marriage
   has_many :pictures
+
+  def as_json(*args)
+    options = args.extract_options!
+    options = {
+      except: [:created_at, :updated_at]
+    }.merge(options)
+
+    super(*args, options).merge(
+      snap_url: pictures.first.try(:snap_url)
+    )
+  end
 end
+
