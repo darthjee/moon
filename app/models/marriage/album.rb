56fcdcac130bb45ff748c5af0a2a8d2d8132ab08
darthjee <darthjee@gmail.com>
2016-03-26 00:07:14 -0300

    Add new scopes

--- app/models/marriage/album.rb
+++ app/models/marriage/album.rb
@@ -3,6 +3,8 @@ class Marriage::Album < ActiveRecord::Base
   has_many :pictures
 
   scope :displayable, proc { where.not(status: :cancelled) }
+  scope :from_album, proc { |album_id| where(album_id: album_id) }
+  scope :top_album, proc { where(album_id: nil) }
   default_scope { displayable }
 
   def as_json(*args)
