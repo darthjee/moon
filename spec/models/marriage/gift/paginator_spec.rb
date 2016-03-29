065f6028dcd70a523b6a3ad4eee907bdde4bb5bf
darthjee <darthjee@gmail.com>
2016-03-29 00:24:35 -0300

    Remove repeated spec

--- spec/models/marriage/gift/paginator_spec.rb
+++ spec/models/marriage/gift/paginator_spec.rb
@@ -27,14 +27,6 @@ describe Marriage::Gift::Paginator do
     let(:gifts_json) { subject.as_json[:gifts] }
     let(:gifts) { Marriage::Gift.where(marriage_id: marriage) }
 
-    it 'returns all the pagination information' do
-      expect(subject.as_json.keys).to eq([:gifts, :pages, :page])
-    end
-
-    it 'returns all the gifts from the marriage' do
-      expect(gifts_json).to eq(gifts.as_json)
-    end
-
     context 'when marriage has more gifts than each page can hold' do
       let(:last_gifts) { 2.times.map { create(:gift, marriage: marriage) } }
       let(:first_gifts) { per_page.times.map { create(:gift, marriage: marriage) } }
@@ -46,18 +38,6 @@ describe Marriage::Gift::Paginator do
         first_gifts.each(&:thread)
         per_page.times.map { create(:gift, marriage: marriage) }.each(&:thread)
         last_gifts.each(&:thread)
-      end
-
-      it 'returns the first page gifts only' do
-        expect(gifts_json).to eq(first_gifts.as_json)
-      end
-
-      context 'when requesting last page' do
-        let(:page) { 3 }
-
-        it 'returns the last page gifts only' do
-          expect(gifts_json).to eq(last_gifts.as_json)
-        end
       end
 
       context 'when there are gifts to be ordered by name' do
