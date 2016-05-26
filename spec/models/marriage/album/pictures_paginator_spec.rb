292bb62cd0005ecf673c3d46ed3dc439d407cdf7
darthjee <darthjee@gmail.com>
2016-05-26 15:41:43 -0300

    Add spec for pics + albums

--- spec/models/marriage/album/pictures_paginator_spec.rb
+++ spec/models/marriage/album/pictures_paginator_spec.rb
@@ -1,49 +1,71 @@
 require 'spec_helper'
 
 describe Marriage::Album::PicturesPaginator do
+  let(:marriage) { create(:marriage) }
+  let(:album) { create(:album, marriage: marriage) }
+  let(:subalbums) { album.albums }
+  let(:pictures) { album.pictures }
+  let(:first_documents) { documents.limit(per_page) }
+  let(:last_documents) { documents.last(documents.count % per_page) }
+
   context 'when album has only subalbums' do
     it_behaves_like 'a paginator', described_class, :itens do
       let(:subject) { described_class.new(documents, pictures, params) }
-      let(:marriage) { create(:marriage) }
-      let(:album) { create(:album, marriage: marriage) }
       let(:documents) { subalbums }
-      let(:subalbums) { album.albums }
-      let(:pictures) { album.pictures }
       let(:documents_with_10_itens) do
-        create(:marriage).tap do |marriage|
+        album.tap do |album|
           10.times.each { create(:album, marriage: marriage, album: album) }
         end.albums
       end
       let(:documents_with_more_pages) do
-        create(:marriage).tap do |marriage|
-          (per_page * 2 + 2).times.each { create(:album, marriage: marriage) }
+        album.tap do |album|
+          (per_page * 2 + 2).times.each { create(:album, marriage: marriage, album: album) }
         end.albums
       end
-      let(:first_documents) { documents.limit(per_page) }
-      let(:last_documents) { documents.last(documents.count % per_page) }
     end
   end
     
   context 'when album has only pictures' do
     it_behaves_like 'a paginator', described_class, :itens do
       let(:subject) { described_class.new(subalbums, documents, params) }
-      let(:marriage) { create(:marriage) }
-      let(:album) { create(:album, marriage: marriage) }
       let(:documents) { pictures }
-      let(:subalbums) { album.albums }
-      let(:pictures) { album.pictures }
       let(:documents_with_10_itens) do
-        create(:album).tap do |album|
+        album.tap do |album|
           10.times.each { create(:picture, album: album) }
         end.pictures
       end
       let(:documents_with_more_pages) do
-        create(:album).tap do |album|
+        album.tap do |album|
           (per_page * 2 + 2).times.each { create(:picture, album: album) }
         end.pictures
       end
-      let(:first_documents) { documents.limit(per_page) }
-      let(:last_documents) { documents.last(documents.count % per_page) }
+    end
+  end
+
+  context 'when album has both subalbums and pictures' do
+    let(:subject) { described_class.new(subalbums, documents, params) }
+    let(:pictures) do
+      album.tap do |album|
+        pictures_count.times.each { create(:picture, album: album) }
+      end.pictures
+    end
+    let(:subalbums) do
+      album.tap do |album|
+        albums_count.times.each { create(:album, marriage: marriage, album: album) }
+      end.albums
+    end
+
+    context 'when pictures and albums fit in a single page' do
+      let(:per_page) { 8 }
+      let(:params) { { per_page: per_page }  }
+      let(:pictures_count) { per_page / 2 }
+      let(:albums_count) { per_page / 2 }
+      let(:subject) { described_class.new(subalbums, pictures, params) }
+      let(:documents_json) { subject.as_json[:itens]  }
+
+      it 'returns both albums and pictures' do
+        expect(documents_json.count).to eq(pictures_count + albums_count)
+      end
     end
   end
 end
