42d44a838d74865a198fcfa417ee7a1de77badce
darthjee <darthjee@gmail.com>
2017-11-27 16:34:40 +0100

    Correc db port usage

--- bin/tests.sh
+++ bin/tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # make sure Postgres is available
-until nc -z postgres 5432; do echo Waiting for Posgres; sleep 1; done
+until nc -z postgres $MOON__POSTGRES_PORT; do echo Waiting for Posgres; sleep 1; done
 
 bundle exec rake db:drop db:create db:schema:load db:migrate
 bundle exec rspec -fd
