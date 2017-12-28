5e906f78096267532da9a822704fb93290aaa73c
darthjee <darthjee@gmail.com>
2017-12-28 18:58:13 +0100

    Add raven js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require raven-js
 //= require rollbar
 //= require jquery
 //= require jquery_ujs
