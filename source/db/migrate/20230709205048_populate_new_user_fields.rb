89cd70ce6be99c296529333c485a1bce6298b186
darthjee <darthjee@gmail.com>
2023-07-09 18:05:34 -0300

    populate new fields

--- source/db/migrate/20230709205048_populate_new_user_fields.rb
+++ source/db/migrate/20230709205048_populate_new_user_fields.rb
@@ -1,4 +1,24 @@
 class PopulateNewUserFields < ActiveRecord::Migration[5.2]
-  def change
+  def up
+    query = <<-SQL
+      UPDATE users
+      SET
+        encrypted_password=password,
+        salt=""
+      WHERE password IS NOT NULL
+    SQL
+
+    execute(query)
+  end
+
+  def down
+    query = <<-SQL
+      UPDATE users
+      SET
+        encrypted_password=NULL,
+        salt=NULL
+    SQL
+
+    execute(query)
   end
 end
