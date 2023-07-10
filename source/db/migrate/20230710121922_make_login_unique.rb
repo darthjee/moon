e795f17dfa9046b8607e9627d2f10b17f3889bcd
darthjee <darthjee@gmail.com>
2023-07-10 10:38:25 -0300

    Stule fix

--- source/db/migrate/20230710121922_make_login_unique.rb
+++ source/db/migrate/20230710121922_make_login_unique.rb
@@ -3,8 +3,8 @@
 class MakeLoginUnique < ActiveRecord::Migration[5.2]
   def change
     change_table :users do |t|
-      t.index :login, unique: :true
-      t.index :email, unique: :true
+      t.index :login, unique: true
+      t.index :email, unique: true
     end
   end
 end
