a734324974b3492b01cbec86b058590164d7a77b
darthjee <darthjee@gmail.com>
2023-07-10 08:41:01 -0300

    Populcate login

--- source/db/schema.rb
+++ source/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_09_195909) do
+ActiveRecord::Schema.define(version: 2023_07_10_112815) do
 
   create_table "bank_accounts", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "bank_id"
