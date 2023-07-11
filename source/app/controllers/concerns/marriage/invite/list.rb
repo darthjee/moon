97e9d7ff5d9c355f3586b043dc0e2571259ac7bd
darthjee <darthjee@gmail.com>
2023-07-11 18:52:54 -0300

    Fix class definition

--- source/app/controllers/concerns/marriage/invite/list.rb
+++ source/app/controllers/concerns/marriage/invite/list.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Marriage
-  class Invite < ActiveRecord::Base
+  class Invite < ApplicationRecord
     module List
       extend ActiveSupport::Concern
 
--- source/app/controllers/concerns/marriage/picture/common.rb
+++ source/app/controllers/concerns/marriage/picture/common.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Marriage
-  class Picture < ActiveRecord::Base
+  class Picture < ApplicationRecord
     module Common
       extend ActiveSupport::Concern
       include ::Marriage::Common
