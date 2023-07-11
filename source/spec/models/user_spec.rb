74170ec70f98b57e8e730fffa408072d575a79f9
darthjee <darthjee@gmail.com>
2023-07-11 16:22:58 -0300

    Add user spec

--- source/spec/models/user_spec.rb
+++ source/spec/models/user_spec.rb
@@ -3,6 +3,10 @@
 require 'spec_helper'
 
 describe User do
+
+  it_behaves_like 'an object that has a secure random code start up',
+                  :code, :authentication_token
+
   describe '.login' do
     let!(:user)    { create(:user, password: password) }
     let(:password) { 'my_custom_password' }
@@ -36,6 +40,40 @@ describe User do
       it do
         expect { described_class.login(**login_hash) }
           .to raise_error(Moon::Exception::LoginFailed)
+      end
+    end
+  end
+
+  describe '.create' do
+    let(:user) { build(:user, code: nil, authentication_token: nil) }
+
+    it do
+      expect { user.save }.to change(user, :code)
+    end
+
+    it do
+      expect { user.save }.to change(user, :authentication_token)
+    end
+  end
+
+  describe 'validations' do
+    let(:user) { users(:second_second) }
+
+    context 'when email is nil' do
+      it { expect(user).to be_valid }
+    end
+
+    context 'when email is not nil' do
+      let(:invite) { users(:with_email) }
+
+      context 'and is valid' do
+        it { expect(user).to be_valid }
+      end
+
+      context 'and is invalid ' do
+        before { user.email = 'wrong_email' }
+
+        it { expect(user).to be_invalid }
       end
     end
   end
