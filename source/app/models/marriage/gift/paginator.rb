d38ce99a3b8464481bb828fc1d1afee4a501063e
darthjee <darthjee@gmail.com>
2024-06-20 17:10:05 -0300

    Add models

---
+++ source/app/models/marriage/gift/paginator.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Marriage
+  class Gift < ActiveRecord::Base
+    class Paginator < Utils::Paginator
+      private
+
+      def key
+        :gifts
+      end
+
+      def order_by
+        case params[:sort_by]
+        when 'price'
+          sort_desc? ? :max_price : :min_price
+        else
+          :name
+        end
+      end
+
+      def sort_desc?
+        sort_direction == 'desc'
+      end
+
+      def sort_direction
+        params[:sort_direction].present? ? params[:sort_direction] : :asc
+      end
+
+      def ordered_list
+        paginated_list
+          .order(order_by => sort_direction.to_sym)
+          .order(name: :asc)
+      end
+    end
+  end
+end
