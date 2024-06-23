5c6cb0b202897f99a70a383c72de150e9b9a3512
darthjee <darthjee@gmail.com>
2024-06-23 15:05:36 -0300

    Attribute is optional

--- source/app/models/marriage/guest.rb
+++ source/app/models/marriage/guest.rb
@@ -3,7 +3,7 @@
 module Marriage
   class Guest < ActiveRecord::Base
     belongs_to :invite
-    belongs_to :marriage
+    belongs_to :marriage, optional: true
 
     scope(:confirmed, proc { where(presence: true) })
     scope(:search_name, proc { |name| where('name LIKE ?', "%#{name}%") })
