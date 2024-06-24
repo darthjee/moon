fbda0b2be5c0583c7a00f385ed6423d5c6c510c7
darthjee <darthjee@gmail.com>
2024-06-24 19:09:58 -0300

    Add seed

--- source/db/seeds.rb
+++ source/db/seeds.rb
@@ -3,4 +3,12 @@
 # needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed
 # command (or created alongside the database with db:setup).
-#
+
+Zyra
+  .register(Marriage::Marriage, find_by: :id)
+
+Zyra.find_or_create(
+  :marriage_marriage,
+  id: 1,
+  display_name: "Test Marriage"
+)
