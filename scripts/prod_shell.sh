5f3775d691377a61bdba7e78882b6b6c50b9e5d6
darthjee <darthjee@gmail.com>
2024-07-02 19:39:54 -0300

    Add docker up

--- scripts/prod_shell.sh
+++ scripts/prod_shell.sh
@@ -13,6 +13,17 @@ function run_docker() {
   docker-compose run moon_production /bin/bash
 }
 
+function up() {
+  SERVICE_ID=$(service_id)
+
+  setup_env $SERVICE_ID
+  clean_env & up_docker
+}
+
+function up_docker() {
+  docker-compose up moon_production
+}
+
 function clean_env() {
   sleep 10
   echo "" > .env.production
@@ -29,6 +40,9 @@ ACTION=$1
 case $ACTION in
   "run")
     run
+    ;;
+  "up")
+    up
     ;;
   *)
     $ACTION
